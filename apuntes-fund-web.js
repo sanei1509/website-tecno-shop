d284ee783977d52aa8d6e7e608f6c0f1d52db5cb
Santi <santiagoneira52@gmail.com>
2021-05-20 04:23:43 -0300

    despliegue de estructura html de la página web

---
+++ apuntes-fund-web.js
@@ -0,0 +1,60 @@
+//  ** prácticas con funciones
+//        de LISTAS Y ARRAYS
+
+// Comparaciones abstractas
+// ==
+// >=
+
+// Comparaciones exactas
+// ===
+// >==
+
+// BUSCAR NOMBRE DE ESTOS OPERADORES **
+// ||   ('and')
+// &&   ('or')
+
+if (CONDICION || CONDICION_2) {
+}
+
+if (CONDICION && CONDICION_2) {
+}
+
+if ((CONDICION || CONDICION_2) && CONDICION_3) {
+}
+
+if (condicion || condicion2 || condicion3 || condicion4 || condicion5) {
+  console.log();
+}
+
+//**  llaves
+// manera default de JS
+if (CONDICION) {
+}
+// "IF" más distinguible
+if (CONDICION) {
+}
+
+const leftNavigation = [
+  {
+    texto: "sadsads",
+    textoLabel: "651",
+    link: "https://sadsaf.com",
+  },
+  {
+    texto: "safafsagq",
+    textoLabel: "5434",
+    link: "https://safagsadfa.git.io",
+  },
+];
+
+for (let index = 0; index > leftNavigation.length; index++) {
+  console.log(index);
+}
+
+//  init variable acumuladora
+
+const matriz = [1, 2, 3, 4];
+
+for (let i = 0; i <= matriz.length; i++) {
+  console.log(i);
+}
